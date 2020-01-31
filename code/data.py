1b1f95ee539e19a10408add4f828e1e425840fec
yuan1202 <li_yuan@outlook.com>
2020-01-31 18:23:52 +0000

    Study bunch of public code.

--- code/data.py
+++ code/data.py
@@ -9,74 +9,21 @@ from torch.utils.data.dataset import Dataset
 from config import *
 
 
-class DatasetMixin(Dataset):
+class DS_TRN(Dataset):
 
-    def __init__(self, transform=None):
-        self.transform = transform
+    def __init__(self, img_arr, lbl_arr, sampling_weight=None, transform=None):
+        self.img_arr            = img_arr
+        self.labels             = lbl_arr
+        self.transform          = transform
 
     def __getitem__(self, index):
-        """Returns an example or a sequence of examples."""
-        if torch.is_tensor(index):
-            index = index.tolist()
-        if isinstance(index, slice):
-            current, stop, step = index.indices(len(self))
-            return [self.get_example_wrapper(i) for i in
-                    six.moves.range(current, stop, step)]
-        elif isinstance(index, list) or isinstance(index, numpy.ndarray):
-            return [self.get_example_wrapper(i) for i in index]
-        else:
-            return self.get_example_wrapper(index)
+        """Returns an example or a sequence of examples from each population."""
+        return self.img_lst[index], self.labels[index]
 
     def __len__(self):
         """Returns the number of data points."""
-        raise NotImplementedError
-
-    def get_example_wrapper(self, i):
-        """Wrapper of `get_example`, to apply `transform` if necessary"""
-        example = self.get_example(i)
-        if self.transform:
-            example = self.transform(example)
-        return example
-
-    def get_example(self, i):
-        """Returns the i-th example.
-
-        Implementations should override it. It should raise :class:`IndexError`
-        if the index is invalid.
-
-        Args:
-            i (int): The index of the example.
-
-        Returns:
-            The i-th example.
-
-        """
-        raise NotImplementedError
+        self.img_lst.shape[0]
 
 
-class BengaliAIDataset(DatasetMixin):
-    def __init__(self, images, labels=None, transform=None, indices=None):
-        super(BengaliAIDataset, self).__init__(transform=transform)
-        self.images = images
-        self.labels = labels
-        if indices is None:
-            indices = np.arange(len(images))
-        self.indices = indices
-        self.train = labels is not None
-
-    def __len__(self):
-        """return length of this dataset"""
-        return len(self.indices)
-
-    def get_example(self, i):
-        """Return i-th data"""
-        i = self.indices[i]
-        x = self.images[i]
-        # Opposite white and black: background will be white and
-        # for future Affine transformation
-        x = (255 - x).astype(np.float32) / 255.
-        if self.train:
-            y = self.labels[i]
-            return x, y
-        else:
-            return x
+class DS_TST(Dataset):
+    pass
